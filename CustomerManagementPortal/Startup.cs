5c129ac33c1b977f0bbdf920a13387beff86dd5c
Takahito Iwasa <iwasa-t@pise.co.jp>
2017-02-09 13:16:44 +0900

    Add project files.

---
+++ CustomerManagementPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerManagementPortal.Startup))]
+namespace CustomerManagementPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
